b1c6bc51a80021a518627465f41ddeea7b24397f
THATONEKO <thatoneko03@gmail.com>
2024-06-01 12:44:22 +0200

    second commit

--- test_publisher/test_publisher/robot_publisher.py
+++ test_publisher/test_publisher/robot_publisher.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 """
 Script to autonomously navigate Robot
@@ -18,9 +19,11 @@ class AutonomousRobot(Node):
             LaserScan, "/scan", self.lidar_callback, 10
         )
         self.cmd = Twist()
-        self.safe_distance = 0.5  # Distance to avoid obstacles in meters
+        self.safe_distance = 0.9  # Distance to avoid obstacles in meters
         self.obstacle_on_left = False
         self.obstacle_on_right = False
+        self.adjust = 0
+        self.move_again = 1.0 
 
     def lidar_callback(self, msg):
         # Check for obstacles in front of the robot
@@ -33,14 +36,16 @@ class AutonomousRobot(Node):
         self.obstacle_on_left = min(left_scan) < self.safe_distance
         self.obstacle_on_right = min(right_scan) < self.safe_distance
 
-        self.get_logger().info(f"Left scan: {left_scan}")
-        self.get_logger().info(f"Right scan: {right_scan}")
+        self.get_logger().info(f"right scan: {left_scan}")
+        self.get_logger().info(f"left scan: {right_scan}")
 
     def run(self):
         while rclpy.ok():
             if self.obstacle_on_left or self.obstacle_on_right:
+                self.get_logger().info(f"obstacle there")
                 self.avoid_obstacle()
             else:
+                self.get_logger().info(f"no obstacle")
                 self.move_forward()
             rclpy.spin_once(self)
 
@@ -54,12 +59,28 @@ class AutonomousRobot(Node):
         if self.obstacle_on_left:
             self.cmd.angular.z = 0.5  # Turn right if there's an obstacle on the left
             self.get_logger().info(f"turning left: {self.cmd.angular.z}")
+            self.adjust += self.cmd.angular.z
+            self.get_logger().info(f"total rotation: {self.adjust}")
+            if self.obstacle_on_left == False:
+                self.cmd.linear.x = self.move_again
+                self.back_to_path(self)
         elif self.obstacle_on_right:
             self.cmd.angular.z = -0.5  # Turn left if there's an obstacle on the right
             self.get_logger().info(f"turning right: {self.cmd.angular.z}")
+            self.adjust -= self.cmd.angular.z
+            self.get_logger().info(f"total rotation: {self.adjust}")
+            if self.obstacle_on_right == False:
+                self.cmd.linear.x = self.move_again
+                self.back_to_path(self)
         self.publisher.publish(self.cmd)
 
+    def back_to_path(self):
+        self.cmd.angular.x = 0.0
+        self.cmd.angular.z = -(self.adjust)
+        self.adjust = 0
+        self.publisher.publish(self.cmd)
 
+            
 def main(args=None):
     rclpy.init(args=args)
     autonomous_robot = AutonomousRobot()
